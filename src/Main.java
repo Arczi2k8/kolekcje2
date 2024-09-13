493a6bea363c288f6cf50f980f69e9ca8d0ee0a3
Arczi2k8 <artur.rachubi@gmail.com>
2024-09-13 11:35:30 +0200

    sito greka

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,38 @@ public class Main {
         Gra gra1 = new Gra();
         gra1.zagraj();
 
+        System.out.println("liczby pierwsze" + sito(200));
+
     }
 
+    //do kartkówkki
+
+    static ArrayList<Integer> sito(int liczba){
+        //przygotowanie listy
+        ArrayList<Boolean> logiczne = new ArrayList<>();
+        logiczne.add(false);
+        logiczne.add(false);
+        for (int i = 0; i < liczba; i++) {
+            logiczne.add(true);
+        }
+
+        //wykreslanie wielokrotnosci
+        for (int i = 0; i < logiczne.size(); i++) {
+            if (logiczne.get(i).equals(true)){
+                //wykrweslanie zmiana na false
+                for (int j = i*2; j < logiczne.size(); j =j + i) {
+                    logiczne.set(j, false);
+                }
+            }
+
+        }
+        ArrayList<Integer> pierwsze = new ArrayList<>();
+        for (int i = 0; i < logiczne.size(); i++) {
+            if (logiczne.get(i).equals(true)){
+                pierwsze.add(i);
+            }
+        }
+        return pierwsze;
+    }
 
 }
